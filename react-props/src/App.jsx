2dbc653b7d85fa8ea34b0ce96990ada2ea045920
GreatnessGrace <graceunstoppabel7@gmail.com>
2024-02-14 10:10:45 +0530

    [Completed] React Props

---
+++ react-props/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+  let myObj = {
+    username: 'Grace',
+    age: 23
+  }
+
+  return (
+    <>
+    <h1 className='bg-green-500 mb-5'>Is Tailwind Working ?</h1>
+    <Card name='Grace' someObj= {myObj} btnText='click me'/>
+    <Card name='Unstoppable' btnText='visit me'/>
+
+    </>
+  )
+}
+
+export default App
